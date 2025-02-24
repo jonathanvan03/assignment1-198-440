e43a64435ab4bf0ce875a415055456d67626f4a6
Christine Yue <138889270+117o3@users.noreply.github.com>
2025-02-23 21:22:59 -0500

    docstring & tie breaking

--- src/Cell.py
+++ src/Cell.py
@@ -1,28 +1,55 @@
 class Cell:
+    larger_g = False  # Class-level variable for tie-breaking strategy
 
-    larger_g = False
-
-    def __init__(self, x, y, g, h, parent = None):
+    def __init__(self, x, y, g, h, parent=None):
+        """
+        Initialize a cell.
+        :param x: x-coordinate of the cell
+        :param y: y-coordinate of the cell
+        :param g: cost from the start to this cell
+        :param h: heuristic value (estimated cost to the goal)
+        :param parent: parent cell in the path
+        """
         self.x = x
         self.y = y
-        self.g = g
-        self.h = h
-        self.f = g + h
-        self.parent = parent
+        self.g = g  # Cost from start to this cell
+        self.h = h  # Heuristic (Manhattan distance to goal)
+        self.f = g + h  # Total cost (f = g + h)
+        self.parent = parent  # Pointer to the parent cell
 
     def __lt__(self, other):
+        """
+        Defines how cells are compared in the priority queue.
+        :param other: another cell to compare with
+        :return: True if this cell has higher priority, False otherwise
+        """
         if self.f == other.f:
+            # Tie-breaking: prefer larger g-values if larger_g is True, else prefer smaller g-values
             return self.g > other.g if Cell.larger_g else self.g < other.g
+        # Default: prefer smaller f-values
         return self.f < other.f
-    
-    def __eq__(self, other):
-        return (self.x, self.y) == (other.x, other.y)
-    
-    def __hash__(self):
-        return hash((self.x, self.y))
-    
-    def update(self, new_g, new_parent):
-        self.g = new_g
-        self.f = self.g + self.h
-        self.parent = new_parent
 
+    def __eq__(self, other):
+        """
+        Defines equality between two cells based on their coordinates.
+        :param other: another cell to compare with
+        :return: True if the cells have the same coordinates, False otherwise
+        """
+        return (self.x, self.y) == (other.x, other.y)
+
+    def __hash__(self):
+        """
+        Defines the hash value of a cell based on its coordinates.
+        :return: hash value of the cell
+        """
+        return hash((self.x, self.y))
+
+    def update(self, new_g, new_parent):
+        """
+        Update the cell's g-value, f-value, and parent.
+        :param new_g: new g-value (cost from start to this cell)
+        :param new_parent: new parent cell in the path
+        """
+        self.g = new_g
+        self.f = self.g + self.h  # Update f-value
+        self.parent = new_parent  # Update parent
